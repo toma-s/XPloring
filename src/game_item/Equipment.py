72cd62aa5c410a7e5931391f2ffe04d44b68ebf0
TheTimeBandit <tomixtt@gmail.com>
2019-06-08 09:10:56 +0200

    Equipment extends Items, added actions to equipment

--- src/game_item/Equipment.py
+++ src/game_item/Equipment.py
@@ -1,6 +1,8 @@
-class Equipment:
+from game_item.Item import Item
+
+
+class Equipment(Item):
     def __init__(self, data: dict):
-        self.alias = data['alias']
-        self.description = data['description']
+        super().__init__(data)
         self.slot = data['slot']  # what equipment slot does this game_item fit
         self.in_use = data['in_use']  # is game_item currently equipped
