741e77c94648f1947341d256bb7ad9fbbd205ef0
TheTimeBandit <tomixtt@gmail.com>
2019-06-09 22:30:26 +0200

    add test for 'drop <item>' from inventory

--- tests/TestInventory.py
+++ tests/TestInventory.py
@@ -56,3 +56,111 @@ class TestInventory(unittest.TestCase):
         expected_output = "sword - Sword made of pure silver with a straight " \
                           "double-edged blade and a grip for two-handed use\n"
         self.assertEqual(expected_output, result_output)
+
+    def test_inventory_sword_equipped(self):
+        self.ih.handle_user_input("take sword")
+        self.ih.handle_user_input("equip sword")
+        stdout = io.StringIO()
+        with contextlib.redirect_stdout(stdout):
+            self.ih.handle_user_input("inventory")
+        result_output = stdout.getvalue()
+        expected_output = "sword - Sword made of pure silver with a straight " \
+                          "double-edged blade and a grip for two-handed use [EQUIPPED]\n"
+        self.assertEqual(expected_output, result_output)
+
+    def test_inventory_two_armour(self):
+        self.ih.handle_user_input("take gladiator helmet")
+        self.ih.handle_user_input("equip steel helmet")
+        self.ih.handle_user_input("take steel chestplate")
+        self.ih.handle_user_input("equip chestplate")
+        self.assertEqual("#equipment_steel_chestplate", self.game_state.hero.chest)
+        self.assertEqual("#equipment_gladiator_helmet", self.game_state.hero.head)
+        stdout = io.StringIO()
+        with contextlib.redirect_stdout(stdout):
+            self.ih.handle_user_input("inventory")
+        result_output = stdout.getvalue()
+        expected_output = "helmet - Gladiator helmet made of steel [EQUIPPED]\n" \
+                          "chestplate - Steel chestplate armor [EQUIPPED]\n"
+        self.assertEqual(expected_output, result_output)
+
+    def test_inventory_unequip_two_armour(self):
+        self.ih.handle_user_input("take gladiator helmet")
+        self.ih.handle_user_input("equip steel helmet")
+        self.ih.handle_user_input("take steel chestplate")
+        self.ih.handle_user_input("equip chestplate")
+        self.ih.handle_user_input("unequip steel chestplate")
+        self.ih.handle_user_input("unequip gladiator helmet")
+        self.assertEqual(None, self.game_state.hero.chest)
+        self.assertEqual(None, self.game_state.hero.head)
+        stdout = io.StringIO()
+        with contextlib.redirect_stdout(stdout):
+            self.ih.handle_user_input("inventory")
+        result_output = stdout.getvalue()
+        expected_output = "helmet - Gladiator helmet made of steel\n" \
+                          "chestplate - Steel chestplate armor\n"
+        self.assertEqual(expected_output, result_output)
+
+    def test_inventory_unequip_two_armour_then_equip(self):
+        self.ih.handle_user_input("take gladiator helmet")
+        self.ih.handle_user_input("equip steel helmet")
+        self.ih.handle_user_input("take steel chestplate")
+        self.ih.handle_user_input("equip chestplate")
+        self.ih.handle_user_input("unequip steel chestplate")
+        self.ih.handle_user_input("unequip gladiator helmet")
+        self.ih.handle_user_input("equip gladiator helmet")
+        self.assertEqual(None, self.game_state.hero.chest)
+        self.assertEqual("#equipment_gladiator_helmet", self.game_state.hero.head)
+        stdout = io.StringIO()
+        with contextlib.redirect_stdout(stdout):
+            self.ih.handle_user_input("inventory")
+        result_output = stdout.getvalue()
+        expected_output = "helmet - Gladiator helmet made of steel [EQUIPPED]\n" \
+                          "chestplate - Steel chestplate armor\n"
+        self.assertEqual(expected_output, result_output)
+
+
+    def test_inventory_drop_sword(self):
+        self.ih.handle_user_input("take sword")
+        self.assertEqual(1, len(self.game_state.hero.inventory))
+        self.ih.handle_user_input("drop sword")
+        stdout = io.StringIO()
+        with contextlib.redirect_stdout(stdout):
+            self.ih.handle_user_input("inventory")
+        result_output = stdout.getvalue()
+        expected_output = "Your inventory is empty.\n"
+        self.assertEqual(expected_output, result_output)
+        self.assertEqual(0, len(self.game_state.hero.inventory))
+
+    def test_inventory_equip_drop_sword(self):
+        self.ih.handle_user_input("take sword")
+        self.assertEqual(1, len(self.game_state.hero.inventory))
+        self.ih.handle_user_input("equip sword")
+        self.assertEqual("#equipment_silver_sword", self.game_state.hero.right_hand)
+        self.ih.handle_user_input("drop sword")
+        stdout = io.StringIO()
+        with contextlib.redirect_stdout(stdout):
+            self.ih.handle_user_input("inventory")
+        result_output = stdout.getvalue()
+        expected_output = "Your inventory is empty.\n"
+        self.assertEqual(expected_output, result_output)
+        self.assertEqual(0, len(self.game_state.hero.inventory))
+
+    def test_inventory_equip_drop_two_armour(self):
+        self.ih.handle_user_input("take gladiator helmet")
+        self.ih.handle_user_input("equip steel helmet")
+        self.ih.handle_user_input("take steel chestplate")
+        self.ih.handle_user_input("equip chestplate")
+        self.assertEqual("#equipment_steel_chestplate", self.game_state.hero.chest)
+        self.assertEqual("#equipment_gladiator_helmet", self.game_state.hero.head)
+        self.assertEqual(2, len(self.game_state.hero.inventory))
+        self.ih.handle_user_input("drop gladiator helmet")
+        self.ih.handle_user_input("drop helmet")
+        self.ih.handle_user_input("drop steel chestplate")
+        stdout = io.StringIO()
+        with contextlib.redirect_stdout(stdout):
+            self.ih.handle_user_input("inventory")
+        result_output = stdout.getvalue()
+        expected_output = "Your inventory is empty.\n"
+        self.assertEqual(expected_output, result_output)
+        self.assertEqual(0, len(self.game_state.hero.inventory))
+
