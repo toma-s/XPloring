1f8cdbaf4b5c382aab9de0a5ade41bb3f16541bd
Jakub Valent <jakub.valent@gmail.com>
2019-06-13 08:20:59 +0200

    gui refactoring

--- src/GUI.py
+++ src/GUI.py
@@ -14,7 +14,7 @@ class GUI:
         self.window.config(bg='lightgrey')
         self.window.geometry(str(width) + 'x' + str(height))
         self.window.title("XPloring game")
-        self.window.bind('<Return>', self.enterPressed)
+        self.window.bind('<Return>', self.enter_pressed)
 
         self.main_frame = tkinter.Frame(self.window)
         self.main_frame.place(relx=0.5, rely=0.5, anchor=tkinter.CENTER)
@@ -32,27 +32,27 @@ class GUI:
         self.text_input.delete('1.0', tkinter.END)
         self.text_input.config(fg='black')
 
-    def getInput(self):
+    def get_input(self):
         input_text = self.text_input.get("1.0", "end-1c")
         self.text_input.delete('1.0', tkinter.END)
         return input_text
 
-    def setOutput(self, text):
+    def set_output(self, text):
         self.text_output.configure(state='normal')
         self.text_output.insert(tkinter.INSERT, text)
         self.text_output.insert(tkinter.INSERT, ">>> ")
         self.text_output.configure(state='disabled')
         self.text_output.see("end")
 
-    def setInputToOutput(self, text):
+    def set_input_to_output(self, text):
         self.text_output.configure(state='normal')
         self.text_output.insert(tkinter.END, text)
         self.text_output.configure(state='disabled')
         self.text_output.see("end")
 
-    def enterPressed(self, e):
-        input = self.getInput()
-        self.setInputToOutput(input)
+    def enter_pressed(self, e):
+        input = self.get_input()
+        self.set_input_to_output(input)
         self.game.react_to_input(input)
 
 class GamePickerGUI:
@@ -64,7 +64,7 @@ class GamePickerGUI:
         self.window.config(bg='lightgrey')
         self.window.geometry(str(width) + 'x' + str(height))
         self.window.title("X-Ploring game")
-        self.window.bind('<Return>', self.enterPressed)
+        self.window.bind('<Return>', self.enter_pressed)
 
         self.main_frame = tkinter.Frame(self.window)
         self.main_frame.place(relx=0.5, rely=0.5, anchor=tkinter.CENTER)
@@ -79,10 +79,10 @@ class GamePickerGUI:
         self.choices = set(choices_arr)
         self.tkvar.set(choices_arr[0])  # set the default option
 
-        popupMenu = tkinter.OptionMenu(self.main_frame, self.tkvar, *self.choices)
-        popupMenu.grid(column=1, row=0)
+        popup_menu = tkinter.OptionMenu(self.main_frame, self.tkvar, *self.choices)
+        popup_menu.grid(column=1, row=0)
 
-    def enterPressed(self, e):
+    def enter_pressed(self, e):
         try:
             GameState(join(parent_path,self.tkvar.get()))
             self.retun_val = self.tkvar.get()
